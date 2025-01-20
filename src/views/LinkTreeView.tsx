73691d516a767199452a156eb94b4d3e757270de
pakomercado0517 <sissyyayle0517@hotmail.com>
2025-01-20 12:15:47 -0600

    change logo icon and useEffect warnings

--- src/views/LinkTreeView.tsx
+++ src/views/LinkTreeView.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from "react";
 import { social } from "../data/social";
 import DevTreeInput from "../components/DevTreeInput";
